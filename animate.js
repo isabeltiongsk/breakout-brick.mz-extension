2bce0ba69d6eae0b3cd30de42ffb533b9474b5d4
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-06-14 12:46:03 +0800

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+function draw(){
+    
+    
+    requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
